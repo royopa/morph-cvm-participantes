7da72bea1fa42ef7d0075c9a839598f69ed01691
Rodrigo Prado de Jesus <royopa@gmail.com>
2020-10-04 18:16:32 -0300

    ajustado print

--- scraper.py
+++ scraper.py
@@ -1,28 +1,37 @@
 # -*- coding: utf-8 -*-
-from __future__ import print_function
+import os
+import time
+
+import lxml.html
 os.environ['SCRAPERWIKI_DATABASE_NAME'] = 'sqlite:///data.sqlite'
 import scraperwiki
-import lxml.html
-import os
 
-os.environ['SCRAPERWIKI_DATABASE_NAME'] = 'sqlite:///data.sqlite'
 
-# # Read in a page
+
 for pk_partic in range(99999, 999999):
-    print(str(pk_partic), end=' ')
+    print(pk_partic)
     url = 'http://cvmweb.cvm.gov.br/SWB/Sistemas/SCW/CPublica/DemContabeis/CPublicaDemContabeisFI.aspx?PK_PARTIC={}'
     pk_partic = str(pk_partic)
-    
+
     try:
         html = scraperwiki.scrape(url.format(pk_partic))
     except Exception as e:
-        continue
-	    
+        time.sleep(15)
+        try:
+            html = scraperwiki.scrape(url.format(pk_partic))
+        except Exception as e:
+            time.sleep(15)
+            try:
+                html = scraperwiki.scrape(url.format(pk_partic))
+            except Exception as e:
+                continue
+
     # Find something on the page using css selectors
     root = lxml.html.fromstring(html)
 
     try:
-        cnpj = root.get_element_by_id("lbNrPfPj").text.replace('.', '').replace('-', '').replace('/', '')    
+        cnpj = root.get_element_by_id("lbNrPfPj").text.replace(
+            '.', '').replace('-', '').replace('/', '')
     except Exception as e:
         continue
 
